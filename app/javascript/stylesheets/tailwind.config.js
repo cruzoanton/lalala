b02e72236bee77515ecf95c6d9d63ccede3c8377
Sergii Brytiuk <brytiuk076@gmail.com>
2021-06-08 12:14:59 +0300

    login template

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,11 +1,41 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                green: {
+                    "busmon-dark": '#3B837C'
+                },
+                secondblack: '#626466'
+            },
+            width: {
+                'auth': '540px'
+            },
+            height: {
+                'auth': '554px'
+            }
+        },
+        left: {
+            'auth': '450px'
+        },
+        fontFamily: {
+            sans: ['Kodchasan', 'sans-serif']
+        },
+        fontSize: {
+            heading: ['32px', '42px'],
+            subheading: ['16px', '24px'],
+            label: ['14px', '18px'],
+            link: ['14px', '22px']
+        },
+        fontWeight: {
+            normal: 400,
+            bold: 700,
+            semibold: 600
+        },
+        variants: {
+            extend: {},
+        },
+        plugins: [],
+    }
 }
